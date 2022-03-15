111c49c09697f66b7cd64b18e3d29c2fd4ea21ed
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-03-15 08:25:08 -0700

    Initial Commit

---
+++ Light Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light Project
+//
+//  Created by Taiwo Adekanmbi on 2022-03-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
