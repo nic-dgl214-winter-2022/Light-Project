17cae4c81c43950f229aa11ef57d432c12e97fe4
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-03-15 08:57:34 -0700

    Update the button text

--- Light Project/ViewController.swift
+++ Light Project/ViewController.swift
@@ -10,23 +10,27 @@ import UIKit
 class ViewController: UIViewController {
     var lightOn = true
 
+    @IBOutlet var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
     fileprivate func updateUI() {
         if lightOn {
             view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
         } else {
             view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
         }
     }
     
     @IBAction func buttonPressed(_ sender: Any) {
         lightOn.toggle()
         updateUI()
-       
     }
     
 }
